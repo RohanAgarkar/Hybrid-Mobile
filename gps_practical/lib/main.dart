61dd0fa7f09ead699a1b422bcde50a834d8c45ba
GamerPro-1228 <145771449+GamerPro-1228@users.noreply.github.com>
2024-08-21 15:07:29 +0530

    GPS-PRACTICAL added

--- gps_practical/lib/main.dart
+++ gps_practical/lib/main.dart
@@ -1,22 +1,80 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Google Maps Flutter Demo',
+//       home: MapSample(),
+//     );
+//   }
+// }
+
+// class MapSample extends StatefulWidget {
+//   @override
+//   State<MapSample> createState() => MapSampleState();
+// }
+
+// class MapSampleState extends State<MapSample> {
+//   late GoogleMapController mapController;
+
+//   final LatLng _center = const LatLng(22.2927001,73.1197053); // San Francisco
+
+//   void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Google Maps Flutter Demo'),
+//         backgroundColor: Colors.green[700],
+//       ),
+//       body: GoogleMap(
+//         onMapCreated: _onMapCreated,
+//         initialCameraPosition: CameraPosition(
+//           target: _center,
+//           zoom: 11.0,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';  // Import for kIsWeb
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
-  State createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Maps Flutter Demo',
+      home: MapSample(),
+    );
+  }
 }
 
-class _MyAppState extends State {
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
   late GoogleMapController mapController;
 
-
-  final LatLng _center = const LatLng(22.29301284148933, 73.12225873737414);
+  final LatLng _center = const LatLng(37.7749, -122.4194); // Example location
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -24,24 +82,20 @@ class _MyAppState extends State {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green[500],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps Flutter Demo'),
+        backgroundColor: Colors.green[700],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Navrachana University'),
-          elevation: 2,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 8.0,
-          ),
-        ),
-      ),
+      body: kIsWeb
+          ? HtmlElementView(viewType: 'google-maps')
+          : GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),
     );
   }
 }
